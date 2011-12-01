d7e0ea31aa874dd970621b331e6f1ac591e48a3c
Surendran Mahendran <surenspost@gmail.com>
2011-12-01 12:43:24 +0530

    Do DEBUG build by default

--- build_secexec.sh
+++ build_secexec.sh
@@ -1,7 +1,7 @@
 cd src/checker/cc_backend/se/
-#gcc -DDBG -DGETOPT -DJAIL -I. -o secexec secexec.c
+gcc -DDBG -DGETOPT -DJAIL -I. -o secexec secexec.c
 #gcc -DDBG -DJAIL -I. -o secexec secexec.c
-gcc -I. -o secexec secexec.c
+#gcc -I. -o secexec secexec.c
 sudo chown root:root secexec
 sudo cp secexec /usr/local/bin
 sudo chmod u+s secexec
